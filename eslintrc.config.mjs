3126c7bfa2b60bee02f86f4e583f6d511c5be021
Sven_Packmaster <koliaagapov17@gmail.com>
2025-12-09 19:37:09 +0300

    Лр

--- eslintrc.config.mjs
+++ eslintrc.config.mjs
@@ -1,19 +1,10 @@
 import js from "@eslint/js";
-import globals from "globals";
 
 export default [
   js.configs.recommended,
   {
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: "module",
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
+    plugins: {
+      extends: ["airbnb-base/legacy"]
     },
-    rules: {
-      'no-unused-vars': 'warn'
-    }
   }
 ];
